ffe1a9017761ae28c6fd7557a27f97f9ca082191
LDS <luanddt.dev@gmail.com>
2025-01-23 16:45:57 +0700

    initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from "tailwindcss"
+
+export default {
+  content: [
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["var(--font-poppins)"]
+      },
+      colors: {}
+    }
+  },
+  plugins: []
+} satisfies Config
